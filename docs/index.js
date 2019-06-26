c01bfce621d78df72468ec69ac0c3e907a97bf26
Raphaël Yancey <raphael@badfile.net>
2019-06-26 16:45:59 +0200

    Added pre-filled issue for unexpected results

--- docs/index.js
+++ docs/index.js
@@ -4,12 +4,21 @@ $(document).ready(function() {
   var form = $('#main-form');
   var output = $('#output');
   var input = $('#command');
+  var issueLink = $("#issue-link")
+  var issueURL = new URL("https://github.com/raphaelyancey/cli2compose/issues/new");
+  var issueTemplate = "**Input**\n```\n[input]\n```\n**Output**\n```\n[output]\n```";
   
   $('#command').focus();
 
   function generate(i) {
     var yaml = cli2compose(i);
     output.html(yaml);
+    var tpl = issueTemplate;
+    tpl = tpl.replace('[input]', i);
+    tpl = tpl.replace('[output]', yaml);
+    issueURL.searchParams.append('body', tpl);
+    issueURL.searchParams.append('title', 'Unexpected result');
+    issueLink.attr('href', issueURL.href);
     hljs.highlightBlock(output.get(0));
   }
 
